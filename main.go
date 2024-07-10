88c1c2dad4b1979764723499d727f88e6d6ecded
vinhbh <hoangvinh1577@gmail.com>
2024-07-10 10:26:38 +0700

    Handle JWT encrypt and validate

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"go-api-server/controllers"
 	"go-api-server/initializers"
+	"go-api-server/middlewares"
 	"os"
 
 	"github.com/gin-gonic/gin"
@@ -19,6 +20,10 @@ func main() {
 	r.GET("/posts/:id", controllers.GetPost)
 	r.PUT("/posts/", controllers.UpdatePost)
 	r.DELETE("/posts/:id", controllers.DeletePost)
+
+	r.POST("/signup", controllers.Signup)
+	r.POST("/login", controllers.Login)
+	r.GET("/validate", middlewares.RequireAuth, middlewares.RoleAuth, controllers.Validate)
 	port := os.Getenv("PORT")
 	r.Run(port)
 }
